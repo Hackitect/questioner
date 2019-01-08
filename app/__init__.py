34f97491eb688ecfa881603571a86b58baa7cd24
Charles Njenga <mwangicharles@gmail.com>
2019-01-08 07:47:18 +0300

    [feature#163044268] import meetups blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    
+    from app.api.v1.meetups import meetups
+
+    #register the blueprints
+
+    app.register_blueprint(meetups)
 
     return app
--- app/api/v1/meetups/views.py
+++ app/api/v1/meetups/views.py
@@ -2,9 +2,14 @@ from flask import Blueprint, request, jsonify, jsonify
 import datetime
 from app.api.v1.meetups import models
 
-meetups = Blueprint('meetups', __name__)
+meetups = Blueprint('meetups', __name__, url_prefix='/api/v1')
 
 #post a meetup
-@meetups.route("/api/v1/meetups", methods=['POST'])
+@meetups.route("/meetups", methods=['POST'])
 def post_meetup():
+    pass
+
+#fetch a specific meetup record
+@meetups.route("/meetups/<meetup-id>", methods=['GET'])
+def get_meetup():
     pass
