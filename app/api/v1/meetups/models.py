cb18511f7e70cfe16c954c9c573ca522b9b13b02
Charles Njenga <mwangicharles@gmail.com>
2019-01-08 10:28:43 +0300

    [feature#163044268] finalized get  meetup by id

--- app/api/v1/meetups/models.py
+++ app/api/v1/meetups/models.py
@@ -1,18 +1,18 @@
 meetups_db = [
-    {
+{
     "id": 1,
     "topic": "Andela Hackathon",
     "location": "PAC University",
     "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
     "tags": ["python", "machine learning"]
-},
-    {
+    },
+{
     "id": 2,
-    "topic": "Advanced CSS3,
+    "topic": "Advanced CSS3",
     "location": "The Hub",
     "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
     "tags": ["html", "css"]
-}
+    }
 ]
 
 ''' This is the response spec for a meeting
@@ -31,9 +31,14 @@ meetups_db = [
 '''
 
 class Meetups():
-    def find_meetup_by_id(self, meetupId):
-		for meetup in meetups_db:
-			if meetup['id'] == meetupId:
-				return meetup
-			else:
-				return {"message": "No meetup found with that id"}
+    def find_by_id(self, meetupId):
+        for meetup in meetups_db:
+            if meetup['id'] == meetupId:
+                return meetup
+            else:
+                return {"message": "No meetup found with that id"}
+    
+    def all(self):
+        if len(meetups_db) == 0:
+            return {"message": "empty database"}
+        return meetups_db
