cfcf361220c29bfdd79abee9084c206a74dadb45
Charles Njenga <mwangicharles@gmail.com>
2019-01-09 12:27:21 +0300

    [finish#163044275] excluded vscode settings in

--- app/api/v1/meetups/models.py
+++ app/api/v1/meetups/models.py
@@ -1,28 +1,28 @@
 import datetime
 
 meetups_db = [
-{
-    "id": 4,
-    "topic": "Andela Hackathon",
-    "location": "PAC University",
-    "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
-    "tags": ["python", "machine learning"]
-    },
-{
-    "id": 1,
-    "topic": "Advanced CSS3",
-    "location": "The Hub",
-    "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
-    "tags": ["html", "css"]
-    },
+    {
+        "id": 4,
+        "topic": "Andela Hackathon",
+        "location": "PAC University",
+        "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
+        "tags": ["python", "machine learning"]
+        },
+    {
+        "id": 1,
+        "topic": "Advanced CSS3",
+        "location": "The Hub",
+        "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
+        "tags": ["html", "css"]
+        },
 
     {
-    "id": 0,
-    "topic": "Ubuntu Server Hangout",
-    "location": "The Hub",
-    "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
-    "tags": ["linux", "server"]
-    },
+        "id": 0,
+        "topic": "Ubuntu Server Hangout",
+        "location": "The Hub",
+        "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
+        "tags": ["linux", "server"]
+    }
 
 ]
 
@@ -56,13 +56,16 @@ class Meetups:
 
     @staticmethod
     def find_by_id(meetupId):
-        for meetup in meetups_db:
-            if meetup['id'] == meetupId:
-                return meetup
-            else:
-                return {"message": "No meetup found with that id"}
+        # list comprehension
+        return[meetup for meetup in meetups_db if meetup['id']==meetupId]
+
+        # for meetup in meetups_db:
+        #     if meetup['id'] == meetupId:
+        #         return meetup
+            # else:
+            #     return {"message": "No meetup found with that id"}
     
     def all(self):
         if len(meetups_db) == 0:
             return {"message": "empty database"}
-        return meetups_db
+        return [meetup for meetup in meetups_db]
