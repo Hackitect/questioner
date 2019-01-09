ad2924595b0e3da6c5698482a654ae9489896640
Charles Njenga <mwangicharles@gmail.com>
2019-01-09 15:42:12 +0300

    [feature#163044259] created tests for post question

---
+++ app/api/v1/questions/models.py
@@ -0,0 +1,13 @@
+import datetime
+
+QuestionDB = [
+    {
+        "id": 1,
+        "createdOn": "2019, 1, 8, 7, 50, 55, 529588",
+        "createdBy": 1, # represents the user asking the question
+        "meetup": 1, # represents the meetup the question is for
+        "title": "Andela Workshop",
+        "body": "When are we meeting to discuss the creation of tests?",
+        "votes": 3
+    }
+]
