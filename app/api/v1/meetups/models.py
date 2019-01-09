fda0207c0a679c698d876fe57f8101021f113f9a
Charles Njenga <mwangicharles@gmail.com>
2019-01-09 12:06:26 +0300

    [start#163044275] created the fetch all method in models

--- app/api/v1/meetups/models.py
+++ app/api/v1/meetups/models.py
@@ -1,6 +1,8 @@
+import datetime
+
 meetups_db = [
 {
-    "id": 2,
+    "id": 4,
     "topic": "Andela Hackathon",
     "location": "PAC University",
     "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
@@ -20,7 +22,7 @@ meetups_db = [
     "location": "The Hub",
     "happeningOn": "2019, 1, 8, 7, 50, 55, 529588",
     "tags": ["linux", "server"]
-    }
+    },
 
 ]
 
@@ -39,9 +41,21 @@ meetups_db = [
 }
 '''
 
-class Meetups():
+class Meetups:
+    
+    # def __init__(self, topic, location, tags):
+    #     self.id = len(meetups_db)+1
+    #     self.topic = topic
+    #     self.location = location
+    #     self.tags = tags
 
-    def find_by_id(self, meetupId):
+    def save(self):
+        meetups_db.append(self)
+
+
+
+    @staticmethod
+    def find_by_id(meetupId):
         for meetup in meetups_db:
             if meetup['id'] == meetupId:
                 return meetup
