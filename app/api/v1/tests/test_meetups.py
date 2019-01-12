9ffdbdfdfee1a427f923f1f39b2a7ec03c456a19
Charles Njenga <mwangicharles@gmail.com>
2019-01-12 15:22:10 +0300

    [bug#163165489] testing rsvp

--- app/api/v1/tests/test_meetups.py
+++ app/api/v1/tests/test_meetups.py
@@ -8,34 +8,13 @@ class TestMeetupEndpoint(unittest.TestCase):
         self.app = create_app()
         self.app.testing = True
         self.client = self.app.test_client()
-
-        ''' This is the response spec for a meeting
-        {
-            "status": Integer,
-            "data": [{
-                "id": Integer,
-                "topic": String,
-                "location": String
-                "happeningOn": Date,
-                "tags": [String, String, ....]
-            }
-
-            ]
-        }
-        '''
+        
 
         """ mock data for testing endpoint """
-        self.test_1 = {
-            "data": [{
-                "id": 1,
-                "topic": "Andela Hackathon",
-                "location": "PAC University",
-                "happeningOn": "",
-                "tags": ["python", "machine learning"]
-            }]
-        }
+        
         self.test_2 = {"topic": "Andela Hackathon","location": "PAC University", 
                         "happeningOn": "","tags": ["python", "machine learning"]}
+        self.test_rsvp = {"meetup_id": 1, "topic": "Python", "status": "Yes"}
     
     def test_getmeetup(self):
         """ Test for returning meetups """
@@ -56,7 +35,7 @@ class TestMeetupEndpoint(unittest.TestCase):
     def test_api_get_meetup_by_id(self):
         """ Test for returning meeting using meeting id [will use id = 1] """
 
-        postval = self.client.get('/api/v1/meetups', 
+        response = self.client.get('/api/v1/meetups', 
                                                     data =  json.dumps(self.test_2),
                                                     content_type = "application/json")
         # self.assertEqual(postval.status_code, 200)                                           
@@ -64,7 +43,14 @@ class TestMeetupEndpoint(unittest.TestCase):
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertIn('The Hub', str(result))
-      
+    
+    def test_api_rsvp_meeting(self):
+        """ test for RSVP meeting """
+        response = self.client.post('/meetups/1/rsvps', 
+        data = json.dumps(self.test_rsvp), content_type = "application/json")
+        # result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 201)
+        # self.assertIn("Python", str(result))      
 if __name__ == '__main__':
     unittest.main()
         
