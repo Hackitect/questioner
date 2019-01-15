765f1e17ff658fac75b189048c0dccc694dc1e6e
Charles Njenga <mwangicharles@gmail.com>
2019-01-15 13:54:19 +0300

    [chore#163217922] creates the questioner database and tests connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask
+import psycopg2
 
-
+    
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = ""
+
+    try:
+        connection = psycopg2.connect(
+            user = "questioner",
+            password = "password123",
+            host = "localhost",
+            port = '5432',
+            database  = "questioner"
+        )
+        cursor = connection.cursor
+    except (Exception, psycopg2.Error) as Error:
+        return Error
+
     from app.api.v1.meetups.views import meetups
     from app.api.v1.questions.views import questions
+
+     
+    
 
     #register the blueprints
 
