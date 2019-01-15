74530505d3774e9e5e712b1c3cbd431722577488
Charles Njenga <mwangicharles@gmail.com>
2019-01-15 14:27:38 +0300

    [finish#163217922] finishes postgres database integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import psycopg2
+from psycopg2 import Error
 
     
 def create_app():
@@ -8,22 +9,22 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = ""
 
     try:
-        connection = psycopg2.connect(
+        db = psycopg2.connect(
             user = "questioner",
             password = "password123",
             host = "localhost",
             port = '5432',
             database  = "questioner"
         )
-        cursor = connection.cursor
-    except (Exception, psycopg2.Error) as Error:
-        return Error
+        
+    except (Exception, psycopg2.DatabaseError) as Error:
+        raise Error
 
     from app.api.v1.meetups.views import meetups
     from app.api.v1.questions.views import questions
 
      
-    
+    # db.init_app(app)
 
     #register the blueprints
 
