1caed36f6ac8dd7508f892c352259416d7b27d57
Charles Njenga <mwangicharles@gmail.com>
2019-01-15 16:02:13 +0300

    [feature#163222074] adds code to run the test_auth test

---
+++ app/api/v2/tests/test_auth.py
@@ -0,0 +1,44 @@
+import unittest
+from app import create_app
+from flask import jsonify, json
+import pytest
+
+class TestAuthEndpoint(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app.testinig = True
+        self.client = self.app.test_client()
+        self.date = datetime.datetime.now()
+
+        """ mock data for testing endpoint """
+        self.test_signup = {
+                "firstname" : "Charles",
+                "lastname" : "Mwangi",
+                "othername" : "Njenga",
+                "email" : "mwangicharles@gmail.com",
+                "phoneNumber" : "0722867603",
+                "username" : "cnjenga",
+                "registered" : self.date,
+                "isAdmin" : 1      
+                }
+    
+    def test_signup(self):
+        """ Test for posting a question record. """
+        response = self.client.post('/api/v2/auth/signup', 
+                                                    data =  json.dumps(self.test_signup),
+                                                    content_type = "application/json")
+        result = json.loads(response.data.decode('utf-8')) 
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('mwangicharles@gmail.com', str(result))
+    
+    def test_signin(self):
+        response = self.client.patch('/api/v2/auth/login', 
+        data=json.dumps(self.test_signin), 
+        content_type = "application/json")
+        self.assertEqual(response.status_code, 201)
+        
+    
+       
+if __name__ == '__main__':
+    unittest.main()
+        
