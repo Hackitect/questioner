e0cc5d4733bcaa02a1da7444d8db7f5ff823bec3
Charles Njenga <mwangicharles@gmail.com>
2019-01-16 08:03:49 +0300

    [feature#163218841] move the db routines to database.py file in utils

--- app/api/v2/users/models.py
+++ app/api/v2/users/models.py
@@ -10,11 +10,20 @@ try:
     host = "localhost",
     port = "5432",
     database = "questioner"
-)
+    )
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT VERSTION()')
+    db_version = cursor.fetchone()
+    print (db_version)
 except (Exception, psycopg2.DatabaseError) as error:
     raise error
 finally:
-    if conn:
+    if (conn):
+        cursor.close()
         conn.close()
 
 #Create the users model
+
+def create_user_table():
+    pass
