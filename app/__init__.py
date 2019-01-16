3e8f7d3bc32921bceff846b9692477d2affd25d6
Charles Njenga <mwangicharles@gmail.com>
2019-01-16 13:24:58 +0300

    [chore#163252178] register the auth route in app

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ def create_app():
 
     from app.api.v1.meetups.views import meetups
     from app.api.v1.questions.views import questions
+    from app.api.v1.users.views import auth
 
     #register the blueprints
 
     app.register_blueprint(meetups)
     app.register_blueprint(questions)
+    app.register_blueprint(auth)
 
     return app
