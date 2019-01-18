6d4b8f3b0396de4e0805068385d562c6907712e0
Charles Njenga <mwangicharles@gmail.com>
2019-01-18 11:31:43 +0300

    [start#163219247] start the delete meetup endpoint

--- app/api/v2/utils/database.py
+++ app/api/v2/utils/database.py
@@ -32,119 +32,103 @@ questioner=# \\dt
 to drop tables that have relationships use DROP TABLE table_name CASCADE;
 
 """
-def db_connect():
-    try:
-        conn = psycopg2.connect(
-        user = "questioner",
-        password = "password2123",
-        host = "localhost",
-        port = "5432",
-        database = "questioner"
-        )
-
-        return conn
-    except (Exception, psycopg2.DatabaseError) as error:
-        raise error
+class Database():
+    
+    def __init__(self):
+        
+        self.host = 'localhost',
+        self.name = 'questioner',
+        self.user = 'questioner',        
+        self.password = 'password123'
+        
+        try:
+            self.conn = psycopg2.connect(
+                host = self.host,
+                dbname = self.name,
+                user = self.user,
+                password = self.password                
+                )
+            
+            self.cursor = self.conn.cursor
+        
+        except (Exception, psycopg2.DatabaseError) as error:
+            raise error
 #Create the tables for the project
 
-def create_tables():
-    ''' create the tables to be used in the projec
-        i.e. users, meetups, questions and answers
-    '''
-    tables = (
-        """
-        CREATE TABLE IF NOT EXISTS users(
-            user_id serial PRIMARY KEY,
-            firstname VARCHAR(20) NOT NULL,
-            lastname VARCHAR(20) NOT NULL,
-            username VARCHAR(20) NOT NULL,
-            phonenumber VARCHAR(15) NOT NULL,
-            email VARCHAR(100) NOT NULL,
-            password VARCHAR(100) NOT NULL,
-            is_admin bool
-        );
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS meetups(
-            meetups_id serial PRIMARY KEY,
-            topic VARCHAR(30) NOT NULL,
-            happeningon TIMESTAMP,
-            tags VARCHAR(100)
-        );
-        """,
+    def create_tables(self):
+        ''' create the tables to be used in the projec
+            i.e. users, meetups, questions and answers
+        '''
+        tables = (
+            """
+            CREATE TABLE IF NOT EXISTS users(
+                user_id serial PRIMARY KEY,
+                firstname VARCHAR(20) NOT NULL,
+                lastname VARCHAR(20) NOT NULL,
+                username VARCHAR(20) NOT NULL,
+                phonenumber VARCHAR(15) NOT NULL,
+                email VARCHAR(100) NOT NULL,
+                password VARCHAR(100) NOT NULL,
+                is_admin bool
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS meetups(
+                meetups_id serial PRIMARY KEY,
+                topic VARCHAR(30) NOT NULL,
+                happeningon TIMESTAMP,
+                tags VARCHAR(100)
+            );
+            """,
 
-        """
-        CREATE TABLE IF NOT EXISTS questions(
-            questions_id serial PRIMARY KEY,
-            createdon TIMESTAMP,
-            title VARCHAR(30) NOT NULL,
-            body VARCHAR(100) NOT NULL,
-            meetupid INTEGER REFERENCES meetups(meetups_id),
-            createdby INTEGER REFERENCES users(user_id)
-            ON UPDATE CASCADE ON DELETE CASCADE
-        );
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS answers(
-            answers_id serial PRIMARY KEY,
-            data VARCHAR NOT NULL
-        );
-        """)
-    conn = None
-    try:
-        conn = db_connect()
-        cursor = conn.cursor()
-        for table in tables:
-            cursor.execute(table)
-        cursor.close()
-        conn.commit()
+            """
+            CREATE TABLE IF NOT EXISTS questions(
+                questions_id serial PRIMARY KEY,
+                createdon TIMESTAMP,
+                title VARCHAR(30) NOT NULL,
+                body VARCHAR(100) NOT NULL,
+                meetupid INTEGER REFERENCES meetups(meetups_id),
+                createdby INTEGER REFERENCES users(user_id)
+                ON UPDATE CASCADE ON DELETE CASCADE
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS answers(
+                answers_id serial PRIMARY KEY,
+                data VARCHAR NOT NULL
+            );
+            """)
+        self.conn = None
+        try:            
+            for table in tables:
+                self.cursor.execute(table)
+            self.cursor.close()
+            self.conn.commit()
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
         
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    
-    finally:
-        if conn is not None:
-            cursor.close()
-            conn.close()
- 
-# #snippets
-#     cursor = conn.cursor()
-#     cursor.execute('SELECT VERSTION()')
-#     db_version = cursor.fetchone()
-#     print (db_version)
-
-def username_exists(username):
-    sql = """SELECT username FROM users;"""
-    conn = db_connect()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if username == result:
-        # username exists
-        return True
-     
-def email_exists(email):
-    sql = """SELECT email FROM users;"""
-    conn = db_connect()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if email == result:
-        # username exists
-        return True
-
-def run_sql(sql):
-    conn = None
-    conn = db_connect()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    return result
-
-def main():
-    create_tables()
-
-# The below line is used to call the module directly from command line to test tables creation
-# python database.py
-if __name__ == ('__main__'):
-    main()
+        finally:
+            if self.conn is not None:
+                self.cursor.close()
+                self.conn.close()
+  
+    # def username_exists(self, username):
+    #     sql = """SELECT username FROM users;"""
+    #     conn = db_connect()
+    #     cursor = conn.cursor()
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     if username == result:
+    #         # username exists
+    #         return True
+        
+    # def email_exists(self, email):
+    #     sql = """SELECT email FROM users;"""
+    #     conn = db_connect()
+    #     cursor = conn.cursor()
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     if email == result:
+    #         # username exists
+    #         return True  
