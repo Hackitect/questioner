648c7fbef8494dd13d0600522c8372b046682470
Charles Njenga <mwangicharles@gmail.com>
2019-01-18 13:26:50 +0300

    [chore#163219247] wrote function to rest return of users

--- app/api/v2/users/models.py
+++ app/api/v2/users/models.py
@@ -3,6 +3,9 @@ from app.api.v2.utils import database
 from app.api.v2.utils.validators import Validators
 from flask import json, jsonify
 
+db = database.Database()
+cursor = db.cursor()
+
 
 """
 questioner=# INSERT INTO users (firstname, lastname, email, username, phonenumber, password)
@@ -18,28 +21,34 @@ questioner=#
 """
 
 class Users(Validators):
-    def __init__(self):
-        pass
+    def all(self):
+
+        sql = "SELECT * FROM users"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        return results
+    # def __init__(self):
+    #     pass
 
             
     def save(self, firstname, lastname, email, username, phonenumber, password):
 
-        '''Before we save the user, verify first if username already exists'''
-        if database.username_exists(username):
-            raise Exception('Username already exist, choose another one')
-        if database.email_exists(email):
-            raise Exception('Email address already taken, use anohe')
-        if self.is_valid_email(email) is False:
-            raise Exception("Error, Email choosen does not meet requirements")
+        # '''Before we save the user, verify first if username already exists'''
+        # if database.username_exists(username):
+        #     raise Exception('Username already exist, choose another one')
+        # if database.email_exists(email):
+        #     raise Exception('Email address already taken, use anohe')
+        # if self.is_valid_email(email) is False:
+        #     raise Exception("Error, Email choosen does not meet requirements")
 
         """ insert a new users into the users table"""
         sql = """INSERT INTO users (firstname, lastname, email,
                 username, phonenumber, password)
                 VALUES('firstname','lastname','email','username','phonenumber', 'password') RETURNING user_id;"""
         
-        database.run_sql(sql)
+        # database.run_sql(sql)
 
-          
+        cursor.execute(sql)
 
         return {
             "message": "User created successfully",
@@ -50,12 +59,11 @@ class Users(Validators):
 
     def login(self, email, password):
         # sql = """SELECT password from users where email= %s;""" %(email)
-        conn = database.db_connect()
-        cursor = conn.cursor        
+              
         cursor.execute("SELECT password from users where email = %s" %(email))
         results = cursor.fetchall()
         cursor.close()
-        conn.close
+        
         if password == results:
             return {"Status": 201, "Message": "User logged in successfully"}
         else:
