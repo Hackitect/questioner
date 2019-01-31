2f9a11c11ac08f7dc61676fdd7916a5cb72ab105
Charles Njenga <Hackitect@users.noreply.github.com>
2019-01-31 08:11:40 +0300

    Update views.py

--- app/api/v2/meetups/views.py
+++ app/api/v2/meetups/views.py
@@ -7,26 +7,3 @@ meetups = Blueprint('meetups', __name__, url_prefix='/api/v2')
 # @admin_required
 def delete():
     pass
-
-
-# def login_required(f):
-#     @wraps(f)
-#     def wrap(*args, **kwargs):
-#         if 'logged_in' in session:
-#             return f(*args, **kwargs)
-#         else:
-#             return ("You need to login first")
-#             # return redirect(url_for('login_page'))
-
-#     return wrap
-
-# def admin_required(f):
-#     @wraps(f)
-#     def wrap(*args, **kwargs):
-#         if 'is_admin' in session:
-#             return f(*args, **kwargs)
-#         else:
-#             return ("You are not authorized to view this page")
-#             # return redirect(url_for('login_page'))
-
-#     return wrap
